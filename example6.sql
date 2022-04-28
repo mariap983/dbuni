c30f4e65dc1fe5960f3d988ef079618f91810953
mariap983 <maria.petrovaa983@gmail.com>
2022-04-28 14:14:41 +0300

    dbuni

---
+++ example6.sql
@@ -0,0 +1,183 @@
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `countries` (
+  `COUNTRY_ID` int(11) DEFAULT NULL,
+  `COUNTRY_NAME` varchar(40) DEFAULT NULL,
+  `REGION_ID` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `countries`
+--
+
+LOCK TABLES `countries` WRITE;
+/*!40000 ALTER TABLE `countries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `countries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departments` (
+  `DEPARTMENT_ID` int(11) NOT NULL DEFAULT '0',
+  `DEPARTMENT_NAME` varchar(30) NOT NULL,
+  `MANAGER_ID` int(11) NOT NULL DEFAULT '0',
+  `LOCATION_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`DEPARTMENT_ID`),
+  UNIQUE KEY `MANAGER_ID` (`MANAGER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dup_countries`
+--
+
+DROP TABLE IF EXISTS `dup_countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dup_countries` (
+  `COUNTRY_ID` int(11) DEFAULT NULL,
+  `COUNTRY_NAME` varchar(40) DEFAULT NULL,
+  `REGION_ID` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dup_countries`
+--
+
+LOCK TABLES `dup_countries` WRITE;
+/*!40000 ALTER TABLE `dup_countries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dup_countries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `EMPLOYEE_ID` int(11) NOT NULL,
+  `FIRST_NAME` varchar(20) DEFAULT NULL,
+  `LAST_NAME` varchar(25) NOT NULL,
+  `EMAIL` varchar(25) NOT NULL,
+  `PHONE_NUMBER` varchar(20) DEFAULT NULL,
+  `HIRE_DATE` date NOT NULL,
+  `JOB_ID` varchar(10) NOT NULL,
+  `SALARY` decimal(8,2) DEFAULT NULL,
+  `COMMISSION_PCT` decimal(2,2) DEFAULT NULL,
+  `MANAGER_ID` int(11) DEFAULT NULL,
+  `DEPARTMENT_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`EMPLOYEE_ID`),
+  KEY `DEPARTMENT_ID` (`DEPARTMENT_ID`),
+  KEY `MANAGER_ID` (`MANAGER_ID`),
+  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`DEPARTMENT_ID`) REFERENCES `departments` (`DEPARTMENT_ID`),
+  CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`MANAGER_ID`) REFERENCES `departments` (`MANAGER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jobs` (
+  `JOB_ID` varchar(10) NOT NULL,
+  `JOB_TITLE` varchar(35) NOT NULL DEFAULT ' ',
+  `MIN_SALARY` decimal(6,0) DEFAULT '8000',
+  `MAX_SALARY` decimal(6,0) DEFAULT NULL,
+  UNIQUE KEY `JOB_ID` (`JOB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jobs`
+--
+
+LOCK TABLES `jobs` WRITE;
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations_table`
+--
+
+DROP TABLE IF EXISTS `locations_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations_table` (
+  `location_id` int(11) DEFAULT NULL,
+  `street_address` varchar(40) DEFAULT NULL,
+  `Postal_code` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations_table`
+--
+
+LOCK TABLES `locations_table` WRITE;
+/*!40000 ALTER TABLE `locations_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locations_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-26 15:25:17
